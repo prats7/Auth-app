51363a9455c3829d49b3038449c830f48c08f1ef
prats7 <pratyushvishal04@gmail.com>
2020-12-03 14:08:50 +0530

    Update views

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', (req,res) => res.send('login'));
+//Login
+router.get('/login', (req,res) => res.render('login'));
+//Register
+router.get('/register', (req,res) => res.render('register'));
 
-router.get('/Register', (req,res) => res.send('reg'));
 
-//router.get('/', (req,res) => res.send('hey'));
 
 module.exports = router;
