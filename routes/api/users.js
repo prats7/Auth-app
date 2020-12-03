7723e2fe4b982d0c797b4276866d243f7a45d8ee
prats7 <pratyushvishal04@gmail.com>
2020-12-03 15:24:54 +0530

    Logout button

--- routes/api/users.js
+++ routes/api/users.js
@@ -87,5 +87,12 @@ router.post('/login', (req, res, next) => {
     })(req, res, next);
   });
 
+// Logout
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+  });
+
 
 module.exports = router;
