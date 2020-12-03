c5e41874032c18c965128477639e7952890c86d2
prats7 <pratyushvishal04@gmail.com>
2020-12-03 12:20:20 +0530

    Connect to mongodb

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+//DB config
+const db = require('./config/mongoose');
+
 const PORT = 5000 || process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
